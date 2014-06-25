0eb6d82e90f3ec8f53d549d42b4f69b6a9e5558a
Zinan Xing <zinan3d@gmail.com>
2014-06-24 20:39:29 -0700

    breakthrough: browser view scroll down when arrow reaches the bottom

--- js/extension.js
+++ js/extension.js
@@ -19,58 +19,53 @@ $(document).ready(function() {
     document.addEventListener('keydown', function(event) {
         switch (event.keyCode) {
             case 9: // tab key pressed
-                if(false == arrow_on) {
+                if(!arrow_on) {
                     event.preventDefault();
-                    $('.arrow-right').remove();
-                    $("td").filter(function() {
-                        return $.text([this]) == index + '.';
-                    }).prev('td').html('<div class="arrow-right"></div>');
+                    move_arrow_to(index);
                     arrow_on = true;
                 }
                 break;
 
             case 40: // down arrow key
-                if(true == arrow_on) {
+                if(arrow_on) {
                     if(index < MAX_INDEX) {
                         event.preventDefault();
                         ++index;
-                        $('.arrow-right').remove();
-                        $("td").filter(function() {
-                            return $.text([this]) == index + '.';
-                        }).prev('td').html('<div class="arrow-right"></div>');
+                        move_arrow_to(index);
                     }
                 }
                 break;
 
             case 38: // up arrow key
-                if(true == arrow_on) {
+                if(arrow_on) {
                     if(index > 1) {
                         event.preventDefault();
                         --index;
-                        $('.arrow-right').remove();
-                        $("td").filter(function() {
-                            return $.text([this]) == index + '.';
-                        }).prev('td').html('<div class="arrow-right"></div>');
+                        move_arrow_to(index);
                     }
                 }
-                break;
-
-            case 13: // enter key
-                event.preventDefault();
-                var link = $("td").filter(function() {
-                    return $.text([this]) == index + '.';
-                }).parent().find('td.title a').attr('href');
-                open_in_background(link);
                 break;
 
             case 27: // esc key
                 arrow_on = false;
                 $('.arrow-right').remove();
+
+                // remove focus
+                $("td").filter(function() {
+                    return $.text([this]) == index + '.';
+                }).parent().find('td.title a').blur();
                 break;
+
+            case 49: // key "1"
+                if(arrow_on) {
+                    index = 1;
+                    move_arrow_to(index);
+                }
             default:
                 break;
         }
 
+        // ABANDONED, reason: stopped hijacking enter key keydown event
         // open link in background by simulating ctrl+click
         // code: http://stackoverflow.com/questions/10812628/open-a-new-tab-in-the-background
         function open_in_background(link){
@@ -81,6 +76,17 @@ $(document).ready(function() {
             evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
                                         true, false, false, true, 0, null);
             a.dispatchEvent(evt);
+        }
+
+        function move_arrow_to(index) {
+            $('.arrow-right').remove();
+            $("td").filter(function() {
+                return $.text([this]) == index + '.';
+            }).prev('td').html('<div class="arrow-right"></div>');
+
+            $("td").filter(function() {
+                return $.text([this]) == index + '.';
+            }).parent().find('td.title a').focus();
         }
 
     });
