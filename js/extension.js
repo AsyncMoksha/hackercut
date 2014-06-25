379eeda8f6308b125a89ddf67205d372c69dd1ca
Zinan Xing <zinan3d@gmail.com>
2014-06-24 19:24:09 -0700

    worked out the layout

--- js/extension.js
+++ js/extension.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+    const max_index = 31; // including the "More" line - next page
+
+    var index = 1; // global arrow index
+
+    // add additional td before index to make space for pointer
+    for(var i = 1; i < 31; ++i) {
+        $("td").filter(function() {
+            return $.text([this]) == i + '.';
+        }).before('<td></td>');
+    }
+
+    // adjust the colspan of the space filler to compensate 
+    // the additional td so that the summary area
+    // shows right underneath title area
+    $('td[colspan=2]').attr("colspan", 3);
+
     document.addEventListener('keydown', function(event) {
         switch (event.keyCode) {
             case 9: // tab key pressed
@@ -8,9 +24,13 @@ $(document).ready(function() {
                 // $('table:nth-of-type(2)').hide();
                 // document.find('table:first').hide();
                 // $('body table:first table:nth-of-type(2)').hide();
-                $("td").filter(function() {
-                    return $.text([this]) == '2.';
-                }).before('<td><div class="arrow-right"> </div></td>');
+                if(index < max_index) {
+                    $('td .arrow-right').remove();
+                    $("td").filter(function() {
+                        return $.text([this]) == index + '.';
+                    }).prev('td').html('<div class="arrow-right"></div>');
+                    ++index;
+                }
                 break;
             default:
                 break;
